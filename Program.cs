b2e7056211b49640dd0fd2451bdebbee1db006f8
Karina <3431645karina@gmail.com>
2022-09-19 22:40:56 +0300

    Revert "hm3"
    This reverts commit 6f0571ea2dec9886497e48096dea9e7d3e07a09d.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,3 @@
-﻿using Module2HM3;
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
 
-public class Program
-{
-    public string Name { get; set; }
-    public static void Main(string[] args)
-    {
-        Cat_family cat = new House_cat(name: "Tosha") ;
-        string name = cat.Name;
-        string breed = cat.Breed;
-        string colour = cat.Colour;
-        string typeoffur = cat.TypeOfFur;
-        cat.Breed = "Scotish fold";
-        cat.Colour = "Grey";
-        cat.TypeOfFur = "Extremely soft";
-        cat.Run();
-        cat.Sleep();
-        cat.Eat();
-        cat.Purr();
-
-    }
-
-    public string Name1 { get; set; }
-    public static void Main1(string[] args)
-    {
-        Creature dog = new Doggy(name1: "Bieber");
-        string name1 = dog.Name1;
-        dog.Run();
-        dog.Sleep();
-        dog.Eat();
-        dog.Purr();
-    }
-}
